b6b81c37261214ec5482e16a3dee49acacfcb576
s-as-as100 <s.arifsaifi000@gmail.com>
2021-03-01 23:14:42 +0530

    add bootstrap classname nd create styles object add some css property nd using in span nd setting attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state= {  //  any data is used in component by using the state object
-        count :0
+        count :0,
+       
 
     }
+     // react is convert into 10 to 10px
+     
     // this keyword is refer to current object
     render() { 
         return (
             ///  react.createElement  // jsx expersion it compile react element
-            <React.Fragment>   
-                <span>{ this.formatCount()} </span>  
-                <button>Increment</button>
+            <React.Fragment> 
+                
+                <span  className="badge bagde-primary m-2">{ this.formatCount()} </span>  
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         )
     }
