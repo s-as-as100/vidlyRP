f89d0d9232b69bf8ddbed3e3aa79d89c07a2d582
s-as-as100 <s.arifsaifi000@gmail.com>
2021-03-01 23:26:08 +0530

    rendering the classes dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,15 +10,24 @@ class Counter extends Component {
      
     // this keyword is refer to current object
     render() { 
+        
+        // let classes = "badge m-2 badge-"; // this two lines convert class extract to method in class
+        // classes+= (this.state.count===0) ? 'warning' : "primary";
         return (
             ///  react.createElement  // jsx expersion it compile react element
             <React.Fragment> 
                 
-                <span  className="badge bagde-primary m-2">{ this.formatCount()} </span>  
+                <span  className={this.getBadgeClasses()}>{ this.formatCount()} </span>  
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         )
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? 'warning' : "primary";
+        return classes;
+    }
+
     formatCount() {
         const {count} = this.state; // object destruction
         return count === 0 ? 'Zero' : count;
