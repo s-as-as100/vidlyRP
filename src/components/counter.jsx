53362829b3700d1a61a72eb87079b53b5919d7d2
s-as-as100 <s.arifsaifi000@gmail.com>
2021-03-01 23:36:11 +0530

    rendring the list nd add unique id inside the rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state= {  //  any data is used in component by using the state object
         count :0,
+        tags : ["tag1", "tag2","tag3"]
        
 
     }
+
+    // warning  each element in an array or iterator should have a unique key because each item in array should be iterated when any 
+    //  wthat element is changed nd what changed in the virtual dom is updated 
      // react is convert into 10 to 10px
      
     // this keyword is refer to current object
@@ -19,6 +23,11 @@ class Counter extends Component {
                 
                 <span  className={this.getBadgeClasses()}>{ this.formatCount()} </span>  
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    {
+                        this.state.tags.map( tag =>  <li key={tag}>{tag}</li> )
+                    }
+                </ul>
             </React.Fragment>
         )
     }
