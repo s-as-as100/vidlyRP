cf6b5d5f570b207c19a37cd7457c6479c9c9b368
s-as-as100 <s.arifsaifi000@gmail.com>
2021-03-03 14:19:04 +0530

    create handle event nd to access the state but it show undefined then u can solve this problem by using bind methods but one of easy way to convert handle increment into arrow function then u can access the state easily

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,9 +3,26 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state= {  //  any data is used in component by using the state object
         count :0,
-        tags : []
+       
        
 
+    };
+
+
+    // bind methods 
+//  constructor(){         if u don't need to use bind method 
+// you can convert handle increment method to arrow function 
+//      super();
+//      this.handleIncrement = this.handleIncrement.bind(this);
+//  }
+   
+
+
+
+
+    handleIncrement = () => {
+        console.log("Increment Clicked", this) // this event handler is not access this state { strict mode is enable } solve 
+        // by using of bind method 
     }
 
     // warning  each element in an array or iterator should have a unique key because each item in array should be iterated when any 
@@ -13,20 +30,21 @@ class Counter extends Component {
      // react is convert into 10 to 10px
      
     // this keyword is refer to current object
-    renderTags(){
-        if (this.state.tags.length === 0) return <p>There are no tags</p>
-         return  <ul>{this.state.tags.map( tag =>  <li key={tag}>{tag}</li> )}</ul>
-    }
+    // renderTags(){
+    //     if (this.state.tags.length === 0) return <p>There are no tags</p>
+    //      return  <ul>{this.state.tags.map( tag =>  <li key={tag}>{tag}</li> )}</ul>
+    // }
     render() { 
         
         // let classes = "badge m-2 badge-"; // this two lines convert class extract to method in class
         // classes+= (this.state.count===0) ? 'warning' : "primary";
         return (
             ///  react.createElement  // jsx expersion it compile react element
-            <React.Fragment> 
-               {this.state.tags.length === 0 && "please create a new tags"}
-                {this.renderTags()}
-            </React.Fragment>
+            <div>
+             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+             <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+
+            </div>
         )
     }
     getBadgeClasses() {
